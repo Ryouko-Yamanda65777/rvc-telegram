45ba8dceeb0c88b4da7b539c6a11fb00426e0f16
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-10-22 22:27:04 +0700

    Create download_rvcmodels.py

---
+++ src/download_rvcmodels.py
@@ -0,0 +1,68 @@
+import json
+import os
+import shutil
+import urllib.request
+import zipfile
+from argparse import ArgumentParser
+
+BASE_DIR = "/content/HRVC"
+mdxnet_models_dir = os.path.join(BASE_DIR, 'mdxnet_models')
+rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
+output_dir = os.path.join(BASE_DIR, 'song_output')
+
+def extract_zip(extraction_folder, zip_name):
+    os.makedirs(extraction_folder)
+    with zipfile.ZipFile(zip_name, 'r') as zip_ref:
+        zip_ref.extractall(extraction_folder)
+    os.remove(zip_name)
+
+    index_filepath, model_filepath = None, None
+    for root, dirs, files in os.walk(extraction_folder):
+        for name in files:
+            if name.endswith('.index') and os.stat(os.path.join(root, name)).st_size > 1024 * 100:
+                index_filepath = os.path.join(root, name)
+
+            if name.endswith('.pth') and os.stat(os.path.join(root, name)).st_size > 1024 * 1024 * 40:
+                model_filepath = os.path.join(root, name)
+
+    if not model_filepath:
+        raise Exception(f'No .pth model file was found in the extracted zip. Please check {extraction_folder}.')
+
+    # move model and index file to extraction folder
+    os.rename(model_filepath, os.path.join(extraction_folder, os.path.basename(model_filepath)))
+    if index_filepath:
+        os.rename(index_filepath, os.path.join(extraction_folder, os.path.basename(index_filepath)))
+
+    # remove any unnecessary nested folders
+    for filepath in os.listdir(extraction_folder):
+        if os.path.isdir(os.path.join(extraction_folder, filepath)):
+            shutil.rmtree(os.path.join(extraction_folder, filepath))
+
+def download_online_model(url, dir_name):
+    try:
+        print(f'[~] Downloading voice model with name {dir_name}...')
+        zip_name = url.split('/')[-1]
+        extraction_folder = os.path.join(rvc_models_dir, dir_name)
+        if os.path.exists(extraction_folder):
+            raise Exception(f'Voice model directory {dir_name} already exists! Choose a different name for your voice model.')
+
+        if 'pixeldrain.com' in url:
+            url = f'https://pixeldrain.com/api/file/{zip_name}'
+
+        urllib.request.urlretrieve(url, zip_name)
+
+        print('[~] Extracting zip...')
+        extract_zip(extraction_folder, zip_name)
+        print(f'[+] {dir_name} Model successfully downloaded!')
+
+    except Exception as e:
+        print(f'[!] Error: {e}')
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Download and extract voice models")
+    parser.add_argument("url", type=str, help="The URL of the model zip file")
+    parser.add_argument("dir_name", type=str, help="The directory name to store the model")
+
+    args = parser.parse_args()
+
+    download_online_model(args.url, args.dir_name)
